3ee462907d1382f162d74297f7c2490d9b423372
José Manuel (Chema) Colmenar <jmcolmenar@users.noreply.github.com>
2024-04-17 12:09:51 +0200

    Fichero main para práctica

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "Agenda.h"
+
+int main() {
+    int opcion;
+    const int MAX_CONTACTOS = 5;
+
+    Agenda agenda = Agenda(MAX_CONTACTOS);
+
+    do {
+        printf("\n --- MENU --- \n");
+        printf("1. Listado de personas\n");
+        printf("2. Nueva persona\n");
+        printf("3. Borrar persona\n");
+        printf("0. SALIR\n");
+
+        printf("\nElija una opción: ");
+        scanf("%i", &opcion);
+
+        switch (opcion) {
+            case 1:
+                agenda.listadoPersonas();
+                break;
+            case 2:
+                agenda.addPersona();
+                break;
+            case 3:
+                agenda.removePersona();
+                break;
+            case 0:
+                printf("\nAdios\n");
+                break;
+            default:
+                printf("\nOpcion incorrecta\n");
+        }
+    } while (opcion != 0);
+
+    return 0;
+}
